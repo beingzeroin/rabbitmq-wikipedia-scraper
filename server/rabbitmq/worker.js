3c97653da6f55699e53d6cf62aeee5f4500e4925
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-3.local>
2018-07-17 15:35:42 -0600

    added try catch block to worker.js

--- server/rabbitmq/worker.js
+++ server/rabbitmq/worker.js
@@ -8,17 +8,17 @@ const config = require('./rabbitConfig').setup
 
 
 rabbot.handle('DB Request', (message) => {
+  try {
     db.storeSearch(message.body.email, 'false')
     message.ack()
+  } catch (error) { message.nack() }
 })
 
 rabbot.handle('Email Request', (message) => {
   try {
     mail.sendEmail(message.body.html, message.body.emailAddress)
     message.ack()
-  } catch (error) {
-    message.nack()
-  }
+  } catch (error) { message.nack()}
 })
 
 rabbot.handle('Scrape Request', (message) => {
